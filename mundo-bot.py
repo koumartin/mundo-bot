3f366a8a8bbce4ca728aa2ba7e85252378c1278a
Martin Koudelka <koudem16@fel.cvut.cz>
2021-03-21 10:51:24 +0100

    Cleaned up connecting to rooms

--- mundo-bot.py
+++ mundo-bot.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 
 client = discord.Client()
+bot = commands.Bot(command_prefix='!')
 
 TOKEN = "ODIyNTk0ODA1NDI2NDIxNzgx.YFUjHA.7FvSQXT6jvQGGxiXeoffRxQTf3U"
 
@@ -18,22 +19,24 @@ async def on_voice_state_update(member, before, after):
         print("Me to " + str(after.channel))
         return
 
-
     if before.channel != after.channel and after.channel is not None:
 
-        try:
-            voice = discord.utils.get(client.voice_clients,  guild=member.guild)
-            if voice is not None:
-                if not voice.is_connected():
-                    await after.channel.connect()
-                else:
-                    await voice.disconnect()
-                    await after.channel.connect()
-            else:
-                await after.channel.connect()
+        # TODO: Later create a queue and service it
+        # Finds current voice_channel in this guild
+        voice = discord.utils.get(client.voice_clients, guild=member.guild)
+        
+        # In case bot isn't connected to a voice_channel yet
+        if voice is None:
+            voice_client = await after.channel.connect()
+        # Else first disconnect bot from current channel and than connect it
+        else:
+            voice_client = await voice.move_to(after.channel)
 
-        except Exception as err:
-            print(err)
+        
+
+
+
+
 
         print(str(member) + " from channel " + str(before.channel) + " to " + str(after.channel))
 
