dfba0f9e45ae5eece28e6f997b28b035619ae17e
Martin Koudelka <koudem16@fel.cvut.cz>
2022-06-19 13:18:20 +0200

    Docstring and single place of truth in position.

--- mundobot_env/mundobot/position.py
+++ mundobot_env/mundobot/position.py
@@ -1,37 +1,45 @@
+"""Enum class of positions in clash with helper functions."""
+from __future__ import annotations
 import enum
+from functools import reduce
+from typing import List, Optional
 
 
 class Position(enum.Enum):
-    TOP = 1
-    JUNGLE = 2
-    MID = 3
-    BOT = 4
-    SUPPORT = 5
-    FILL = 6
-    NOOB = 7
+    """Enum class of positions in the clash."""
+
+    TOP = ["top", "top-1"]
+    JUNGLE = ["jun", "jung", "jungler"]
+    MID = ["mid", "middle"]
+    BOT = ["adc", "bot", "bottom"]
+    SUPPORT = ["sup", "supp", "support"]
+    FILL = ["fill", "👍"]
+    NOOB = ["👎"]
 
     def __str__(self):
         return self.name
 
     @staticmethod
-    def get_position(emoji_name: str):
-        e = emoji_name.lower()
-        if e in ["adc", "bot", "bottom"]:
-            return Position.ADC
-        if e in ["sup", "supp", "support"]:
-            return Position.SUPPORT
-        if e in ["jun", "jung", "jungler"]:
-            return Position.JUNGLE
-        if e in ["mid", "middle"]:
-            return Position.MID
-        if e in ["top", "top-1"]:
-            return Position.TOP
-        if e in ["fill", "👍"]:
-            return Position.FILL
-        if e in ["👎"]:
-            return Position.NOOB
+    def get_position(emoji_name: str) -> Optional[Position]:
+        """Gets Position that correspponds to the emoji name given.
+
+        Args:
+            emoji_name (str): Emoji for which the Position is found.
+
+        Returns:
+            Optional[Position]: If emoji corresponds to a Position,
+            that Position is returned else None.
+        """
+        emoji_name = emoji_name.lower()
+        for position in Position:
+            if emoji_name in position.value:
+                return position
 
     @staticmethod
-    def accepted_reactions():
-        return ["adc", "sup", "jung", "fill", "top", "mid", "top-1", "👍", "👎", "bot", "supp", "jun",
-                "middle", "support", "jungle", "bottom", "jungler"]
+    def accepted_reactions() -> List[str]:
+        """Gets list of all emoji names for which get_position will return value.
+
+        Returns:
+            List[str]: List of emoji names for which get_position will return value.
+        """
+        return reduce(lambda acc, pos: acc + pos.value, Position, [])
